e58a8ea098cc24118e5eeb5aec5d853f9bd57d94
avdunitydev <avdunitydev@users.noreply.github.com>
2017-06-08 18:10:54 +0300

    cw608

--- Assets/608CW/Scripts/CharacterNavMesh.cs
+++ Assets/608CW/Scripts/CharacterNavMesh.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class CharacterNavMesh : MonoBehaviour {
+public class CharacterNavMesh : MonoBehaviour
+{
+	Animator m_Animator;
+	NavMeshAgent m_Agent;
+	Ray m_RayScreen;
+	RaycastHit m_RayHit;
+	Vector3 m_TargetPoint;
 
-
-	void Start () {
-		
+	void Start ()
+	{
+		m_Animator = GetComponent<Animator> ();
+		m_Agent = GetComponent<NavMeshAgent> ();
+		m_TargetPoint = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			m_RayScreen = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (m_RayScreen, out m_RayHit, 300)) {
+				m_TargetPoint = m_RayHit.point;
+			}
+		}
+		m_Agent.SetDestination (m_TargetPoint);
 	}
 }
-s
