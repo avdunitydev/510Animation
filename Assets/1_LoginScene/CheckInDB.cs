31abe8c40e8b64534a8f2a4f1db6f6db28c0a6a5
avdunitydev <avdunitydev@users.noreply.github.com>
2017-06-29 15:15:49 +0300

    629cw

---
+++ Assets/1_LoginScene/CheckInDB.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckInDB : MonoBehaviour
+{
+
+	public void EndEditName_IN (InputField targetField)
+	{
+		if (targetField.text.Length < 4) {
+			Debug.LogError ("Логін повинен містити не менше 4 символів");
+			isOk &= ~IsRegistrationOk.login;
+			IsActivBtnSignUp ();
+		} else {
+			StartCoroutine (FindName (targetField.text));
+		}
+	}
+
+	IEnumerator FindName (string signInName)
+	{
+		Debug.Log ("start find user name ...");
+		WWWForm form = new WWWForm ();
+		form.AddField ("name", signInName);
+
+		WWW www = new WWW ("http://localhost/test2.site/php/findName.php", form);
+		yield return www;
+
+		if (!string.IsNullOrEmpty (www.error)) {
+			Debug.LogError ("Error: name !" + www.error);
+			yield break;
+		}
+		Debug.Log ("... result >> " + www.text);
+		if (www.text.Length == 1) {
+			if (Equals (www.text, "1")) {
+				Debug.LogError ("Такий логін зайнятий виберіть інший");
+				isOk &= ~IsRegistrationOk.login;
+			} else if (Equals (www.text, "0")) {
+				Debug.Log ("User name OK !");
+				isOk |= IsRegistrationOk.login;
+			}
+			IsActivBtnSignUp ();
+		}
+		yield return null;
+	}
+}
---
+++ Assets/620_DB/ScaleButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleButton : MonoBehaviour
+{
+	float scale = 0.3f;
+
+	public void ScaleUp (RectTransform targetButton)
+	{
+		targetButton.localScale += new Vector3 (scale, scale, 0f);
+	}
+
+	public void ScaleExit (RectTransform targetButton)
+	{
+		targetButton.localScale -= new Vector3 (scale, scale, 0f);
+	}
+}
